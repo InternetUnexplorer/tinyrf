a2e9c57eaab17c98ed3c5167673d97457c9e4b0b
InternetUnexplorer <internetunexplorer@gmail.com>
2020-01-23 22:03:24 -0800

    Add some basic messages

--- src/common/message.rs
+++ src/common/message.rs
@@ -1,9 +1,22 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 /// A message sent from the server to the worker
 #[derive(Debug, Deserialize, Serialize)]
-pub enum ServerMessage {}
+pub enum ServerMessage {
+    /// Start a new render
+    StartRender { animation: Uuid, frame: u32 },
+    /// Cancel the current render
+    CancelRender,
+}
 
 /// A message sent from the worker to the server
 #[derive(Debug, Deserialize, Serialize)]
-pub enum WorkerMessage {}
+pub enum WorkerMessage {
+    /// An error occurred while downloading
+    DownloadFailed,
+    /// An error occurred while uploading
+    UploadFailed,
+    /// Rendering finished
+    RenderFinished,
+}
