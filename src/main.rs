07430cb9433ae30d4d63ae9ead145a8192d33169
InternetUnexplorer <internetunexplorer@gmail.com>
2020-01-24 14:18:00 -0800

    Add argument parsing to main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,58 @@ mod worker;
 
 use crate::{server::Server, worker::Worker};
 use log::{error, LevelFilter};
-use std::{env::args, process::exit};
+use std::process::exit;
+use structopt::StructOpt;
 
-const ADDRESS: &str = "localhost";
-const PORT: u16 = 4096;
+#[derive(Debug, StructOpt)]
+#[structopt(name = "tinyrf", about = "A simple Blender render farm")]
+struct Config {
+    /// Hosts a server
+    #[structopt(short = "s", long = "server")]
+    server: bool,
+
+    /// Server address
+    #[structopt(name = "ADDRESS", default_value = "localhost")]
+    address: String,
+
+    /// Server port
+    #[structopt(short = "p", long = "port", default_value = "4049")]
+    port: u16,
+
+    /// Worker name
+    #[structopt(short = "n", long = "name")]
+    name: Option<String>,
+}
 
 fn main() {
+    // Parse command-line arguments
+    let config = Config::from_args();
+
+    // Initialize logging with LevelFilter::Info
     pretty_env_logger::formatted_builder()
-        .filter_level(LevelFilter::Debug)
+        .filter_level(LevelFilter::Info)
         .init();
 
-    match args().nth(1).as_ref().map(|arg| arg.as_str()) {
-        Some("server") => {
-            if let Err(error) = Server::run(ADDRESS, PORT) {
-                error!("{}", error);
-                exit(2);
-            }
-        }
-        Some("worker") => {
-            if let Err(error) = Worker::connect(ADDRESS, PORT) {
-                error!("{}", error);
-                exit(2);
-            }
-        }
-        _ => {
-            eprintln!("expected server or worker");
-            exit(1);
-        }
+    // Run the server or the worker
+    if config.server {
+        run_server(config);
+    } else {
+        run_worker(config);
+    }
+}
+
+/// Run the server, printing an error message on failure
+fn run_server(config: Config) {
+    if let Err(error) = Server::run(&config.address, config.port) {
+        error!("{}", error);
+        exit(2);
+    }
+}
+
+/// Run the worker, printing an error message on failure
+fn run_worker(config: Config) {
+    if let Err(error) = Worker::connect(config.name, &config.address, config.port) {
+        error!("{}", error);
+        exit(2);
     }
 }
